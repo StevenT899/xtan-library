a31a1d62e39126b87fd6975c74f9305858b2d08f
StevenT899 <502845705@qq.com>
2024-10-18 18:29:20 +1100

    Trigger deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/xtan-library/'
-      : '/'
-  }
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/xtan-library/'
+//       : '/'
+//   }
+
+
+
+  export default defineConfig({
+    base: process.env.CF_PAGES
+      ? '/'              
+      : process.env.GITHUB_ACTIONS
+        ? '/xtan-library/'             
+        : '/',                   
+    build: {
+      outDir: 'dist'
+    },
+  });
